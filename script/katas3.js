0bcc0b28208487536c0734718b2f8d4c56e6d23e
Lucas Maclean <contatolucasmaclean@gmail.com>
2022-11-25 11:05:37 -0300

    Segunda tarefa realizada com sucesso!

--- script/katas3.js
+++ script/katas3.js
@@ -2,12 +2,12 @@
 // Exercícios - Katas 3 
 
 
-// Primeira Tarefa - Retornar o "X" percorrendo o array.
+// Primeira Tarefa - Retornar o no console a movimentação do "x" ao longo do array, de sua posição inicial até a final.
 
 let x = ["x", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"]
 
 function xRunning(expansao) {
-    
+
     for (i = 1; i < expansao.length; i++) {
         console.log(expansao);
         if (expansao[i - 1] == "x") {
@@ -20,10 +20,32 @@ function xRunning(expansao) {
 console.log(xRunning(x));
 
 
+// Segunda Tarefa - Retornar um novo Array substituindo as posições das strings por um número representando a contagem de vogais daquelas palavras.
 
+let lista = ["laranja", "abacaxi", "acerola", "morango", "uva", "melancia"];
 
+function vowelsCounter(lista) {
+    let arrayCount = []
+    for (let i = 0; i < lista.length; i++) {
+        let count = 0;
+        for (let j = 0; j < lista[i].length; j++) {
+            switch (lista[i][j]) {
+                case "a":
+                case "e":
+                case "i":
+                case "o":
+                case "u":
+                    count++;
+                    break;
+            }
+        }
+        arrayCount.push(count)
+    }
+    return arrayCount
+}
+console.log(vowelsCounter(lista));
 
-
+// 
 
 
 
