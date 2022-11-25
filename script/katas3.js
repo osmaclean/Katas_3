bf911f2b7239e9f0da054a31fa4fab9bbda20502
Lucas Maclean <contatolucasmaclean@gmail.com>
2022-11-25 10:51:43 -0300

    Primeira tarefa realizada com sucesso!

--- script/katas3.js
+++ script/katas3.js
@@ -2,6 +2,31 @@
 // Exercícios - Katas 3 
 
 
+// Primeira Tarefa - Retornar o "X" percorrendo o array.
+
+let x = ["x", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"]
+
+function xRunning(expansao) {
+    
+    for (i = 1; i < expansao.length; i++) {
+        console.log(expansao);
+        if (expansao[i - 1] == "x") {
+            expansao.pop("x")
+            expansao.unshift("-")
+        }
+    }
+    return expansao
+}
+console.log(xRunning(x));
+
+
+
+
+
+
+
+
+
 
 
 
