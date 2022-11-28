844b01f9bc514dbd63b56c1fcd3afdb81c945241
Lucas Maclean <contatolucasmaclean@gmail.com>
2022-11-28 01:02:35 -0300

    Buscando soluções para resolver essa tarefa (Atualizando)

--- script/katas3.js
+++ script/katas3.js
@@ -8,14 +8,14 @@ let x = ["x", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"]
 
 function xRunning(expansao) {
 
-    for (i = 1; i < expansao.length; i++) {
-        console.log(expansao);
-        if (expansao[i - 1] == "x") {
-            expansao.pop("x")
-            expansao.unshift("-")
-        }
-    }
-    return expansao
+      for (i = 1; i < expansao.length; i++) {
+            console.log(expansao);
+            if (expansao[i - 1] == "x") {
+                  expansao.pop("x")
+                  expansao.unshift("-")
+            }
+      }
+      return expansao
 }
 console.log(xRunning(x));
 
@@ -24,30 +24,52 @@ console.log(xRunning(x));
 
 let lista = ["laranja", "abacaxi", "acerola", "morango", "uva", "melancia"];
 
-function vowelsCounter(lista) {
-    let arrayCount = []
-    for (let i = 0; i < lista.length; i++) {
-        let count = 0;
-        for (let j = 0; j < lista[i].length; j++) {
-            switch (lista[i][j]) {
-                case "a":
-                case "e":
-                case "i":
-                case "o":
-                case "u":
-                    count++;
-                    break;
+function vowelsCounter(listaas) {
+      let arrayCount = []
+      for (let i = 0; i < listaas.length; i++) {
+            let count = 0;
+            for (let j = 0; j < listaas[i].length; j++) {
+                  switch (listaas[i][j]) {
+                        case "a":
+                        case "e":
+                        case "i":
+                        case "o":
+                        case "u":
+                              count++;
+                              break;
+                  }
             }
-        }
-        arrayCount.push(count)
-    }
-    return arrayCount
+            arrayCount.push(count)
+      }
+      return arrayCount
 }
 console.log(vowelsCounter(lista));
 
-// 
+// Terceira Tarefa - Retornar um novo array com esta frase fatiada em 3 em 3 caracteres posicionados ao longo do array.
 
 
+let frase = ["Os três mosqueteiros"];
+
+function stringTripletGroup(divisao) {
+      let devolucao = []
+      for (let i = 0; i = divisao.length; i++) {
+            let contagem = 0;
+            switch (divisao[i]) {
+                  case "Os ":
+                  case "trê":
+                  case "s m":
+                  case "osq":
+                  case "uet":
+                  case "eir":
+                  case "os ":
+                        contagem++
+                        break;
+            }
+            devolucao.push(contagem)
+      }
+return devolucao
+}
+console.log(stringTripletGroup(frase));
 
 
 
